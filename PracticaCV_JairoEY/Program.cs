16f61fdc143431b1a60421a19945ed30e890681c
SOYUNTAL-JXTX17 <a25jairoencyll@iesluiscarrillodesotomayor.org>
2026-02-18 08:50:21 +0100

    Añadir comentarios a la clase Main.

--- PracticaCV_JairoEY/Program.cs
+++ PracticaCV_JairoEY/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // CONSTANTES
+
+            // VARIABLES
+
+            // INICIALIZACIÓN DE VARIABLES
+
+            // ENTRADA
+            
+            // PROCESO
+
+            // SALIDA
+
+            // MÉTODOS
         }
     }
 }
